e63a5bf09ba4dca0089238b4225753aab147bf95
Michael 'Winnduu' D <michael.dorst@gmail.com>
2016-07-02 16:46:36 +0200

    Recovery page finished. generating a RecoveryCode fully functional now. //Recovery Link corrected. Was pointing to the wrong URL for the Request of a new Password.

--- recovery.php
+++ recovery.php
@@ -55,7 +55,7 @@ if(isset($_GET['send']))
                 $empfaenger = $result['email'];
                 $betreff = "neues PW für website";
                 $from = "von mir";
-                $url_passwortcode = 'http://localhost/passwordrecovery.php?userid='.$result['id'].'&code='.$passwortcode;
+                $url_passwortcode = 'http://localhost/request_new_password.php?userid='.$result['id'].'&code='.$passwortcode;
                 $text = 'Hallo '.$result['vorname'].',
                     für deinen Account auf www.php-einfach.de wurde nach einem neuen Passwort gefragt. Um ein neues Passwort zu vergeben, rufe innerhalb der nächsten 24 Stunden die folgende Website auf:
                     '.$url_passwortcode.'
