ee7e5402e7e29078f3a070719047ee07b60cbc7f
Michael 'Winnduu' D <michael.dorst@gmail.com>
2016-07-02 19:46:38 +0200

    Request Page edited, partwise optimized and rewritten for MYSQLI.

--- request_new_password.php
+++ request_new_password.php
@@ -1,5 +1,5 @@
 <?php
-session_start()
+session_start();
 $link = new mysqli_connect("localhost", "root", "", "test");
 
 if(!isset($_GET['userid']) || !isset($_GET['code'])) {
@@ -10,41 +10,39 @@ $userid = $_GET['userid'];
 $code = $_GET['code'];
 
 //Abfrage des Nutzers
-//TODO: Umbauen auf mysqli
-$statement = $pdo->prepare("SELECT * FROM users WHERE id = :userid");
-$result = $statement->execute(array('userid' => $userid));
-$user = $statement->fetch();
+    $statement = mysqli_query($link, "SELECT * FROM users WHERE id = '".$userid."'");
+    $result = mysqli_fetch_assoc($statement);
+    $user = $result; //QnD Test ;)
 
 //Überprüfe dass ein Nutzer gefunden wurde und dieser auch ein Passwortcode hat
 if($user === null || $user['passwortcode'] === null) {
-    die("Es wurde kein passender Benutzer gefunden");
+    die("User not found.");
 }
 
 if($user['passwortcode_time'] === null || strtotime($user['passwortcode_time']) < (time()-24*3600) ) {
-    die("Dein Code ist leider abgelaufen");
+    die("RecoveryCode ist zu lang (24h+)");
 }
 
 
-//Überprüfe den Passwortcode
+//Überprüung des übergebenen RecoveryCodes
 if(sha1($code) != $user['passwortcode']) {
-    die("Der übergebene Code war ungültig. Stell sicher, dass du den genauen Link in der URL aufgerufen hast.");
+    die("Used RecoveryKey was wrong.");
 }
 
-//Der Code war korrekt, der Nutzer darf ein neues Passwort eingeben
-
+//RecoveryCode stimmt überein und ist noch nicht abgelaufen
 if(isset($_GET['send'])) {
     $passwort = $_POST['passwort'];
     $passwort2 = $_POST['passwort2'];
 
     if($passwort != $passwort2) {
-        echo "Bitte identische Passwörter eingeben";
+        echo "Passwords didn't Match";
     } else { //Speichere neues Passwort und lösche den Code //TODO: Umbauen auf MSQLI
         $passworthash = password_hash($passwort, PASSWORD_DEFAULT);
         $statement = $pdo->prepare("UPDATE users SET passwort = :passworthash, passwortcode = NULL, passwortcode_time = NULL WHERE id = :userid");
         $result = $statement->execute(array('passworthash' => $passworthash, 'userid'=> $userid ));
 
         if($result) {
-            die("Dein Passwort wurde erfolgreich geändert");
+            die("Password_Recovery successfull!");
         }
     }
 }
